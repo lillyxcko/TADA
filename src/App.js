5c3912a9860fd91053dad4cef81f02b215192e88
Lilly Ko <lillyxcko@gmail.com>
2024-09-06 12:04:07 -0700

    prevent zoom gesture

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Diagram from './components/Diagram';
 
 const App = () => {
+  
+  useEffect(() => {
+    // Disable pinch-to-zoom
+    const handleTouchMove = (event) => {
+      if (event.scale !== 1) {
+        event.preventDefault();  // Prevent the default zoom behavior
+      }
+    };
+
+    // Attach the event listener when the component mounts
+    document.addEventListener('touchmove', handleTouchMove, { passive: false });
+
+    // Cleanup event listener on component unmount
+    return () => {
+      document.removeEventListener('touchmove', handleTouchMove);
+    };
+  }, []);
+
   return (
     <div className="app-container">
       <Diagram />
