541825a4efa79e2c1e7ab18e70c4f0d99a2c9155
Lilly Ko <lillyxcko@gmail.com>
2024-12-01 19:12:12 -0800

    adjusting feedback sensitivity

--- src/components/LinkProximity.js
+++ src/components/LinkProximity.js
@@ -75,7 +75,7 @@ const LinkProximity = forwardRef(({ links }, ref) => {
       }
     });
 
-    const maxDistance = 200; // Maximum distance for proximity feedback
+    const maxDistance = 30; // Maximum distance for proximity feedback
     const normalizedDistance = closestLinkDistance / maxDistance;
     const exponent = 5; // Adjust this value for sharper increase
     const volume = Math.max(0, 1 - Math.pow(normalizedDistance, exponent));
