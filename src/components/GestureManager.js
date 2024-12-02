81a2b2155bb89ac0c8c6c61d195f05ba2625c588
Lilly Ko <lillyxcko@gmail.com>
2024-12-01 19:04:57 -0800

    cleanup

--- src/components/GestureManager.js
+++ src/components/GestureManager.js
@@ -119,7 +119,7 @@ export const GestureManager = ({ nodeId, nodeValue, infoIndex, r, activeTouches,
           // Second tap ended before 300ms
           console.log(`Second tap ended before 300ms (${duration}ms). Triggering TTS.`);
           if (!isSpeaking && activeTouches.current.size > 0) {
-            const textToSpeak = `${nodeValue[infoIndex.current]}. Held for ${duration} milliseconds.`;
+            const textToSpeak = `${nodeValue[infoIndex.current]}`;
             speakValue(textToSpeak);
   
             infoIndex.current = (infoIndex.current + 1) % nodeValue.length;
